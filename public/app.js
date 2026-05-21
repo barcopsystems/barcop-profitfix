e4d2fe43ef82a5ed44cf4022b44d02a25413b3a2
barcopsystems <barcopsystems@gmail.com>
2026-05-21 18:38:59 -0500

    Update app.js

--- public/app.js
+++ public/app.js
@@ -568,6 +568,7 @@ const App = {
         'sc-help':               ['Help and FAQ', 'Shift Control'],
       };
       const scScreens = {
+        'sc-active-shift': S.ShiftActiveShift,
         'sc-log-shift': S.ShiftLogShift,
         'sc-shift-history': S.ShiftHistory,
         'sc-cash-drop': S.ShiftCashDrop,
